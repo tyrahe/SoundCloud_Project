ab263676e6e2982d57c2becc02043e12a4e15307
Tyra He <tyransfz@gmail.com>
2015-04-12 16:28:30 -0700

    HTML interface
    Add the HTML component for user login page.

--- login.php
+++ login.php
@@ -5,3 +5,42 @@ $_SESSION["user_name"] = $_GET['name'];
 echo "Hello ".$_SESSION["user_name"];
 
 ?>
+
+
+<html>
+<head>
+<title>User Login</title>
+</head>
+<body>
+<form name="frmUser" method="post" action="">
+<table border="0" cellpadding="10" cellspacing="1" width="500" align="center">
+<tr class="tableheader">
+<td align="center" colspan="2">Registered User Login</td>
+</tr>
+<tr class="tablerow">
+<td align="right">Username</td>
+<td><input type="text" name="userName"></td>
+</tr>
+<tr class="tablerow">
+<td align="right">Password</td>
+<td><input type="password" name="password"></td>
+</tr>
+<tr class="tableheader">
+<td align="center" colspan="2"><input type="submit" name="submit" value="Submit"></td>
+</tr>
+</table>
+</form>
+</body>
+<body>
+        <div id="container">
+            <form>
+                <h4>Create Logon</h4>
+                <div class="line"><label for="username">Username: </label><input type="text" id="username" /></div>
+                <div class="line"><label for="pwd">Password: </label><input type="password" id="pwd" /></div>
+                <!-- You may want to consider adding a "confirm" password box also -->
+                <div class="line"><label for="pwd">Conform Password: </label><input type="password" id="pwd" /></div>
+                <div class="line submit"><input type="submit" value="Submit" /></div>
+             </form>
+        </div>
+    </body>
+</html>
